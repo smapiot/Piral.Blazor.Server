fe69ececb2ff04f81aa7bc1bf3f583a50ca1c008
Florian Rappl <mail@florian-rappl.de>
2024-05-02 08:24:31 +0200

    Improved MFLC

--- src/Piral.Blazor.Orchestrator/MicrofrontendLoadContext.cs
+++ src/Piral.Blazor.Orchestrator/MicrofrontendLoadContext.cs
@@ -5,14 +5,35 @@ namespace Piral.Blazor.Orchestrator;
 
 internal class MicrofrontendLoadContext(string name) : AssemblyLoadContext(name, true)
 {
+    private readonly AssemblyLoadContext _root = All.FirstOrDefault(m => m.Name == "root") ?? Default;
+
     protected override Assembly? Load(AssemblyName assemblyName)
     {
-        //string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
-        //if (assemblyPath != null)
-        //{
-        //    return LoadFromAssemblyPath(assemblyPath);
-        //}
-        //return null;
+        var existing = GetExisting(_root.Assemblies, assemblyName);
+
+        if (existing is not null)
+        {
+            return existing;
+        }
+
+        // root and default are different; we must be in an emulator
+        if (_root != Default)
+        {
+            // Let's see if we find this in the default ALC
+            var globalAssembly = GetExisting(Default.Assemblies, assemblyName);
+
+            if (globalAssembly is not null)
+            {
+                // In case we found it we return null to trigger the not found flow
+                return null;
+            }
+        }
+
         return base.Load(assemblyName);
+    }
+
+    private static Assembly? GetExisting(IEnumerable<Assembly> assemblies, AssemblyName name)
+    {
+        return assemblies.FirstOrDefault(m => AssemblyName.ReferenceMatchesDefinition(m.GetName(), name));
     }
 }
