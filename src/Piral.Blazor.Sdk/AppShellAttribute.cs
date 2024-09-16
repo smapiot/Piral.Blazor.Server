0ed25685ca8fb0a6c103a32ecbfac757dcc350ca
Ulrik Farlov-Qvist <49122691+ZwapKillrath@users.noreply.github.com>
2024-09-16 17:29:45 +0200

    Update AppShellAttribute.cs
    Use primary constructor

--- src/Piral.Blazor.Sdk/AppShellAttribute.cs
+++ src/Piral.Blazor.Sdk/AppShellAttribute.cs
@@ -1,12 +1,7 @@
-﻿namespace Piral.Blazor.Sdk;
+namespace Piral.Blazor.Sdk;
 
 [AttributeUsage(AttributeTargets.Assembly)]
-public class AppShellAttribute : Attribute
+public class AppShellAttribute(string name) : Attribute
 {
-    public AppShellAttribute(string name)
-    {
-        Name = name;
-    }
-
-    public string Name { get; }
+    public string Name { get; } = name;
 }
